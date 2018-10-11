9f4eccc58a1e3719b12c9a63b4046aaecda9b9b9
rti56kt <rti56kt@gmail.com>
2018-10-12 01:36:31 +0800

    ver 1.0

---
+++ HW2/0516220-Project2.cpp
@@ -0,0 +1,151 @@
+#include <list>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <json-c/json.h>
+
+using namespace std;
+
+typedef struct{
+    char id[100];
+    char token[100];
+} id_token;
+
+list<id_token> login_users;
+
+int main(int argc, char *argv[]){
+    while(1){
+        char *input_cmd = NULL;
+        char *user = NULL;
+        char *other_arg = NULL;
+        char input_buf[100] = {'\0'};
+        char recv_buf[300];
+        read(STDIN_FILENO, input_buf, 100);
+
+        /* Delete Saved Tokens And Exit The Program. */
+        if(!strcmp(input_buf, "exit\n")){
+            if(login_users.size() != 0) login_users.clear();    // Delete saved tokens
+            break;
+        }
+
+        /* Fetch Command And User From Input */
+        char tmp[500] = {'\0'};
+        if(strstr(input_buf, " ") != NULL){                     // If input has more than one argument
+            input_cmd = strtok(input_buf, " ");                 // Fetch the command eg. register, login, logout, post, etc.
+            user = strtok(NULL, " ");                           // Fetch <user>
+            if(strstr(user, "\n") != NULL) user = strtok(user, "\n");   // If input argument only has <user> and delete "\n"
+            else other_arg = strtok(NULL, "\n");                // If input argument NOT only has <user> and delete "\n"
+
+            if(strcmp(input_cmd, "register") != 0 && strcmp(input_cmd, "login") != 0){  // If the command is NOT "register" and "login" means
+                int find_user = 0;                                                      // second argument must be <user> and must be replaced by token
+                for(list<id_token>::iterator it = login_users.begin(); it != login_users.end(); it++){
+                    if(!strcmp(user, (*it).id)){                // Find tokens that we saved
+                        user = (*it).token;                     // If there's one replace <user> to token
+                        find_user = 1;                          // Flag that we find <user> is in the token list (also means that he is already login)
+                        break;
+                    }
+                }
+                if(!find_user) strcpy(user, " ");               // If the token list doesn't have this <user> means he isn't login, leave <user> empty
+            }
+
+            if(other_arg == NULL){                              // Combine the inputs together and save into tmp
+                snprintf(tmp, sizeof(tmp), "%s %s", input_cmd, user);               // If inputs only have cmd & <user>
+            }else{
+                snprintf(tmp, sizeof(tmp), "%s %s %s", input_cmd, user, other_arg); // If inputs have cmd, <user>, and other arguments
+            }
+        }else{                                                  // If input has just one argument
+            strtok(input_buf, "\n");                            // Delete "\n"
+            strcpy(tmp, input_buf);                             // Save into tmp
+        }
+        char send_msg[strlen(tmp)] = {'\0'};                    // Declare send_msg (It's the string that we'll send to server)
+        strcpy(send_msg, tmp);                                  // Copy tmp to send_msg
+
+        /* Create Socket */
+        int sock_fd = 0;
+        sock_fd = socket(PF_INET, SOCK_STREAM, 0);
+        if(sock_fd == -1) printf("Fail to create socket.\n");
+
+        /* Info of Socket (Server) */
+        struct sockaddr_in sock2sever;
+        if(argc == 1){                                          // Default server setting
+            memset(&sock2sever, 0, sizeof(sock2sever));
+            sock2sever.sin_family = AF_INET;
+            sock2sever.sin_port = htons(8008);
+            inet_pton(AF_INET, "140.113.207.51", &sock2sever.sin_addr.s_addr);
+        }else if(argc == 3){                                    // Optional server setting
+            int port;
+            sscanf(argv[2], "%d", &port);
+            memset(&sock2sever, 0, sizeof(sock2sever));
+            sock2sever.sin_family = AF_INET;
+            sock2sever.sin_port = htons(port);
+            inet_pton(AF_INET, argv[1], &sock2sever.sin_addr.s_addr);
+        }
+
+        /* Connection */
+        int conn_rv = connect(sock_fd, (struct sockaddr *)&sock2sever, sizeof(sock2sever));
+        if(conn_rv == -1) printf("Fail to connect to the server.\n");
+
+        /* Send Command to Server */
+        send(sock_fd, send_msg, sizeof(send_msg), 0);
+        /* Receive Command from Server */
+        recv(sock_fd, recv_buf, sizeof(recv_buf), 0);
+
+        
+        /* Unpack JSON And Print Out */
+        struct json_object *root, *status, *token, *message, *invite, *my_friend, *post;
+        root = json_tokener_parse(recv_buf);
+        if(root == NULL) printf("Parse failed.\n");
+        status = json_object_object_get(root, "status");
+        token = json_object_object_get(root, "token");
+        invite = json_object_object_get(root, "invite");
+        my_friend = json_object_object_get(root, "friend");
+        post = json_object_object_get(root, "post");
+        message = json_object_object_get(root, "message");
+
+        if(invite != NULL && !strcmp(input_cmd, "list-invite") && json_object_get_int(status) == 0){
+                int arraylen = json_object_array_length(invite);
+                for(int i = 0; i < arraylen; i++){
+                    printf("%s\n", json_object_get_string(json_object_array_get_idx(invite, i)));
+                }
+            if(arraylen == 0) printf("No invitation\n");
+        }else if(my_friend != NULL && !strcmp(input_cmd, "list-friend") && json_object_get_int(status) == 0){
+            int arraylen = json_object_array_length(my_friend);
+            for(int i = 0; i < arraylen; i++){
+                printf("%s\n", json_object_get_string(json_object_array_get_idx(my_friend, i)));
+            }
+            if(arraylen == 0) printf("No friends\n");
+        }else if(post != NULL && !strcmp(input_cmd, "receive-post") && json_object_get_int(status) == 0){
+            int arraylen = json_object_array_length(post);
+            for(int i = 0; i < arraylen; i++){
+                struct json_object *id, *post_msg;
+                id = json_object_object_get(json_object_array_get_idx(post, i), "id");
+                post_msg = json_object_object_get(json_object_array_get_idx(post, i), "message");
+                printf("%s: %s\n", json_object_get_string(id), json_object_get_string(post_msg));
+            }
+            if(arraylen == 0) printf("No posts\n");
+        }else{
+            if(token != NULL && !strcmp(input_cmd, "login") && json_object_get_int(status) == 0){
+                id_token new_item;
+                strcpy(new_item.id, user);
+                strcpy(new_item.token, json_object_get_string(token));
+                login_users.push_back(new_item);
+            }
+            if(!strcmp(input_cmd, "logout") && json_object_get_int(status) == 0){
+                for(list<id_token>::iterator it = login_users.begin(); it != login_users.end(); it++){
+                    if(!strcmp(user, (*it).token)){
+                        login_users.erase(it);
+                        break;
+                    }
+                }
+            }
+            printf("%s\n", json_object_get_string(message));
+        }
+    }
+
+    return 0;
+}
